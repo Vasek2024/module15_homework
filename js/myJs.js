38d578860b77452f6df7b981b3c7cdf28dfe74d4
Vasek2024 <p.f1@yandex.ru>
2025-02-28 06:33:17 +0300

    сделал структуру

---
+++ js/myJs.js
@@ -0,0 +1,18 @@
+let formNumber = document.querySelector('.three__form-number'),
+    formData = document.querySelector('.three__form-data'),
+    formBtn = document.querySelector('.three__form-btn'),
+    formTitle = document.querySelector('.three__number-title'),
+
+    dataOne = document.querySelector('.four__data-one'),
+    dataTwo = document.querySelector('.four__data-two'),
+    fourBtn = document.querySelector('.four__form-btn'),
+    numberTitle = document.querySelector('.four__number-title'),
+    numberImg = document.querySelector('.four__number-img')
+
+
+    let fiveNumber = document.querySelector('.five__form-number'),
+        dataFiveOne = document.querySelector('.five__data-one'),
+        dataFiveTwo = document.querySelector('.five__data-two'),
+        fiveBtn = document.querySelector('.five__form-btn'),
+        numberTitleFive = document.querySelector('.five__number-title'),
+        numberImgFive = document.querySelector('.five__number-img')
